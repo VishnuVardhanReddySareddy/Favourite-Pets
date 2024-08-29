84e1cc27766f592b8fb2b8157f62e632b48e6ad5
Vishnu <vishnureddy0010@gmail.com>
2024-08-29 06:29:01 +0530

    initial commit of the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const pets = [
+    {
+      name: "Lenny",
+      age: 2,
+      habits: ["runnig", "sleeping", "playing"],
+      image:
+        "https://cdn.pixabay.com/photo/2023/08/23/03/33/boxer-8207572_1280.jpg",
+    },
+    {
+      name: "Casey",
+      age: 5,
+      habits: ["runnig", "jumping", "fetching"],
+      image:
+        "https://cdn.pixabay.com/photo/2023/06/29/12/28/cats-8096304_1280.jpg",
+    },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Favourite Pets</h1>
+      <ul className="Container">
+        {pets.map((pet, index) => (
+          <li key={index}>
+            <h1>{pet.name}</h1>
+            <h2>
+              {pet.name} is {pet.age} years old
+            </h2>
+            <img src={pet.image} alt="first Pet" width="200px" height="200px" />
+            <h3>Habits</h3>
+            <ul className="Habits">
+              {pet.habits.map((habit, index) => (
+                <li key={index}>{habit}</li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
